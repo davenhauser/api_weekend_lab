cf1639ae2c3ba91e77506d978627d2a607b704eb
david niederhauser <niederhauser.dave@gmail.com>
2016-03-05 16:00:28 -0800

    make 3rdPartyAPI work

--- starter/foursquare_api.js
+++ starter/foursquare_api.js
@@ -11,12 +11,12 @@ var firstplace;
 
 //function printTemperatures()
 
-var findPlace = function(){
-request("https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3?client_id=" +process.env.CLIENT_ID + "&client_secret=" + process.env.CLIENT_SECRET + "&v=20140806&m=foursquare", function (error, response, body) {
+var printLocation = function(){
+request("https://api.foursquare.com/v2/venues/search?client_id=" +process.env.CLIENT_ID + "&client_secret=" + process.env.CLIENT_SECRET + "&client_secret=FC02AUJEN13SHNJOXB5B3YO12KJ3IZMQQPFM0DKE0OCZEFRS&v=20140806&near=Eagle-rock,CA&m=foursquare&query=sushi", function (error, response, body) {
 
   if (!error && response.statusCode == 200) {
     firstplace = JSON.parse(body);
-    var good = firstplace.response.venue.location.address;
+    var good = firstplace.response.venues[0].name
      // eval(locus);
   console.log("THE CURRENT ADDRESS IS: " + good)
   }
@@ -26,7 +26,7 @@ request("https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3?client_id
 
 // module.exports = API;
 module.exports = {
-  findPlace: findPlace
+  printLocation: printLocation
 }
 
-findPlace()
+printLocation();
